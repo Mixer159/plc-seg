1d742a580cd90f357a5a7e6bd599408a2561867b
Maxim Kudela <kudelamaxim@gmail.com>
2024-09-06 12:50:14 +0200

    fda

---
+++ src/pages/hodina1.tsx
@@ -0,0 +1,17 @@
+import { Box, Container, Heading, Text } from "@chakra-ui/react";
+import React from "react";
+
+function hodina1() {
+  return (
+    <>
+      <Container maxW="container.lg" centerContent>
+        <Heading as="h1" size="2xl">
+          První hodina
+        </Heading>
+        <Text></Text>
+      </Container>
+    </>
+  );
+}
+
+export default hodina1;
